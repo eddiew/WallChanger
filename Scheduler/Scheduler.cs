633659047f8ca65aaa7de1d297e117bc03450ec3
unknown <wangeddx@WANGEDDX-MOBL.amr.corp.intel.com>
2014-08-21 11:50:15 -0700

    Notify icon

--- Scheduler/Scheduler.cs
+++ Scheduler/Scheduler.cs
@@ -6,83 +6,38 @@ using WallChanger;
 
 namespace Scheduler
 {
-    partial class Scheduler : Form
+    static class Scheduler
     {
-        private NotifyIcon notifyIcon;
-        private System.ComponentModel.IContainer components;
-        private Button changeWallButton;
-    
-        public Scheduler()
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static void Main(string[] args)
         {
-            InitializeComponent();
+            var wallChangerTask = CreateWallChangerTask();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SysTrayIcon(wallChangerTask));
         }
 
-        public static void Main(string[] args)
+        public static RunningTask CreateWallChangerTask()
         {
             using (var taskService = new TaskService())
             {
+                var existingTask = taskService.FindTask("WallChanger", false);
+                if (existingTask != null)
+                {
+                    return existingTask.Run();
+                }
                 var td = taskService.NewTask();
                 td.RegistrationInfo.Description =
                     "Fetches an image from wallbase.cc and sets it as the desktop background at fixed intervals";
                 td.RegistrationInfo.Author = "Eddie Wang";
                 td.Settings.StartWhenAvailable = true;
-                var biHourly = new TimeTrigger{Repetition = {Interval = TimeSpan.FromMinutes(30)}};
+                var biHourly = new TimeTrigger { Repetition = { Interval = TimeSpan.FromMinutes(30) } };
                 td.Triggers.Add(biHourly);
                 td.Actions.Add(new ExecAction(WindowsOps.ExecutableDirectory + @"\WallChanger.exe"));
-                taskService.RootFolder.RegisterTaskDefinition("WallChanger", td).Run();
+                return taskService.RootFolder.RegisterTaskDefinition("WallChanger", td).Run();
             }
-        }
-
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Scheduler));
-            this.changeWallButton = new System.Windows.Forms.Button();
-            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.SuspendLayout();
-            // 
-            // changeWallButton
-            // 
-            this.changeWallButton.Location = new System.Drawing.Point(12, 12);
-            this.changeWallButton.Name = "changeWallButton";
-            this.changeWallButton.Size = new System.Drawing.Size(103, 23);
-            this.changeWallButton.TabIndex = 0;
-            this.changeWallButton.Text = "Change Wallpaper";
-            this.changeWallButton.UseVisualStyleBackColor = true;
-            this.changeWallButton.Click += new System.EventHandler(this.ChangeWallButton_Click);
-            // 
-            // notifyIcon
-            // 
-            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
-            this.notifyIcon.Text = "WallChanger";
-            this.notifyIcon.Visible = true;
-            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_MouseClick);
-            // 
-            // Scheduler
-            // 
-            this.AutoSize = true;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(163, 79);
-            this.ControlBox = false;
-            this.Controls.Add(this.changeWallButton);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "Scheduler";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.ResumeLayout(false);
-
-        }
-
-        private void ChangeWallButton_Click(object sender, EventArgs e)
-        {
-            Main(null);
-        }
-
-        private void NotifyIcon_MouseClick(object sender, MouseEventArgs e)
-        {
-
         }
     }
 }
---
+++ Scheduler/SysTrayIcon.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32.TaskScheduler;
+
+namespace Scheduler
+{
+    public partial class SysTrayIcon : Form
+    {
+        private readonly Task wallChangerTask;
+        private NotifyIcon notifyIcon;
+        private ContextMenuStrip contextMenuStrip;
+        private ToolStripMenuItem changeToolStripMenuItem;
+        private ToolStripMenuItem autoToolStripMenuItem;
+        private ToolStripMenuItem optionsToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private System.ComponentModel.IContainer components;
+        private bool isAutoChanging; // necessary b/c Task.State throws COMException
+
+        public SysTrayIcon(Task wallChangerTask)
+        {
+            this.wallChangerTask = wallChangerTask;
+            isAutoChanging = true;
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SysTrayIcon));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.changeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipText = "WallChanger";
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "WallChanger";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeToolStripMenuItem,
+            this.autoToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(137, 92);
+            // 
+            // changeToolStripMenuItem
+            // 
+            this.changeToolStripMenuItem.Name = "changeToolStripMenuItem";
+            this.changeToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.changeToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.changeToolStripMenuItem.ShowShortcutKeys = false;
+            this.changeToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.changeToolStripMenuItem.Text = "Change Now";
+            this.changeToolStripMenuItem.ToolTipText = "Change wallpaper now";
+            this.changeToolStripMenuItem.Click += new System.EventHandler(this.changeToolStripMenuItem_Click);
+            // 
+            // autoToolStripMenuItem
+            // 
+            this.autoToolStripMenuItem.Name = "autoToolStripMenuItem";
+            this.autoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.autoToolStripMenuItem.ShowShortcutKeys = false;
+            this.autoToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.autoToolStripMenuItem.Text = "Pause";
+            this.autoToolStripMenuItem.ToolTipText = "Pause auto-change";
+            this.autoToolStripMenuItem.Click += new System.EventHandler(this.autoToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.optionsToolStripMenuItem.ShowShortcutKeys = false;
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            this.optionsToolStripMenuItem.ToolTipText = "Options";
+            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.ShowShortcutKeys = false;
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.ToolTipText = "Quit Wallchanger and stop auto-changing";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // SysTrayIcon
+            // 
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(433, 781);
+            this.ControlBox = false;
+            this.Enabled = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SysTrayIcon";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            // l33t reflection haxx
+            MethodInfo mi = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);
+            mi.Invoke(notifyIcon, null);
+        }
+
+        private void changeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            wallChangerTask.Run();
+            if (!isAutoChanging)
+            {
+                wallChangerTask.Stop();
+            }
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        private void autoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isAutoChanging)
+            {
+                wallChangerTask.Stop();
+                autoToolStripMenuItem.Text = "Resume";
+                autoToolStripMenuItem.ToolTipText = "Resume auto-change";
+                isAutoChanging = false;
+            }
+            else
+            {
+                wallChangerTask.Run();
+                autoToolStripMenuItem.Text = "Pause";
+                autoToolStripMenuItem.ToolTipText = "Pause auto-change";
+                isAutoChanging = true;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            wallChangerTask.Stop();
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
